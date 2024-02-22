6010bbb6051c22e1f6f98a4a82cb7a91905fbf63
sasaco <sasaicco@gmail.com>
2024-02-22 10:44:30 +0900

    udemy 1

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports={
+    credentials:{
+        client_id:"Ej7q07CpEt8UCqRreWLbRmBwUAjOuNqi",
+        client_secret:"XGNdOd79kZemgf3n"
+    },
+    scopes:{
+        internal: ['bucket:create','bucket:read','data:read','data:create','data:write'],
+        public:['viewables:read']
+    }
+}
